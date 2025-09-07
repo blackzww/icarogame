23422a94b56ba6812b6a3aedecaa8405dfd11967
blackzww <143085633+blackzww@users.noreply.github.com>
2025-09-07 18:54:15 -0300

    Create icarogames.lua

---
+++ icarogames.lua
@@ -0,0 +1,12 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Blackzw Hub",
+    LoadingTitle = "Initializing...",
+    LoadingSubtitle = "Created by Blackzw",
+    Theme = "Default",
+    ToggleUIKeybind = Enum.KeyCode.K,
+    ConfigurationSaving = {Enabled = false},
+    Discord = {Enabled = false},
+    KeySystem = false
+})
